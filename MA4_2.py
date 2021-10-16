3df3e0ccdcf07a4488f2385728c89fed863f354d
Glassn1 <dogge1999@hotmail.com>
2021-10-16 02:00:17 +0200

    up

---
+++ MA4_2.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+
+from integer import Integer
+from time import perf_counter as pc
+import matplotlib
+matplotlib.use('Agg')
+
+import matplotlib.pyplot as plt
+
+py_time = []
+cpp_time = []
+
+def main():
+	I = Integer(0)
+	nlst = range(0,45)
+	print(nlst)
+	for i in nlst:
+		I.set(i)
+
+		start = pc()
+		print(I.fib())
+		end = pc()
+		cpp_time.append(end - start)
+
+		start = pc()
+		print(fib_py(i))
+		end = pc()
+		py_time.append(end - start)
+
+	I.set(47)
+	start = pc()
+	print("Test 47 cpp: ", I.fib())
+	end = pc()
+	print("Time for n = 47 with cpp: ", end - start) 
+
+	plt.plot(nlst, cpp_time, 'b.', nlst, py_time, 'r.')
+	plt.xlabel('Iterations (n)')
+	plt.ylabel("Time for fibonacci (s)")
+	plt.savefig('fib_py_cpp.png')
+	
+		
+		
+
+
+
+def fib_py(n):
+	if n <= 1:
+		return n
+	else:
+		return fib_py(n-1) + fib_py(n-2)
+
+if __name__ == '__main__':
+	main()
+
+
---
+++ integer.cpp
@@ -0,0 +1,54 @@
+#include <cstdlib>
+// Integer class 
+
+class Integer{
+	public:
+		Integer(int);
+		int get();
+		void set(int);
+		int fib();
+
+	private:
+		int val;
+		int _fib(int);
+	};
+
+
+Integer::Integer(int n){
+	val = n;
+	}
+
+int Integer::fib(){
+	int n = val;
+	return _fib(n);
+}
+
+int Integer::_fib(int n){
+	if (n  <= 1){
+		return n;
+	}else{
+		return _fib(n-1) + _fib(n-2);
+	}
+}
+
+int Integer::get(){
+	return val;
+	}
+ 
+void Integer::set(int n){
+	val = n;
+	}
+
+
+extern "C"{
+	Integer* Integer_new(int n) {return new Integer(n);}
+	int Integer_get(Integer* integer) {return integer->get();}
+	int Integer_fib(Integer* integer) {return integer->fib();}
+	void Integer_set(Integer* integer, int n) {integer->set(n);}
+	void Integer_delete(Integer* integer){
+		if (integer){
+			delete integer;
+			integer = nullptr;
+			}
+		}
+	}
